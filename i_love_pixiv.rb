a04625428ef4a31f04508bb29dbfe2fd2502fcee
shimobayashi <akimasa@shimobayashi.name>
2014-01-18 14:15:34 +0900

    divide monkey patch

--- i_love_pixiv.rb
+++ i_love_pixiv.rb
@@ -10,15 +10,7 @@ require_relative 'simple_illust_ids_fetcher'
 require_relative 'famous_illust_ids_fetcher'
 require_relative 'recommended_illust_ids_fetcher'
 require_relative 'utils'
-
-module Pixiv
-  class Illust
-    lazy_attr_reader(:illust_id) { at!('link[rel="alternate"][hreflang="ja"]')[:href][/illust_id=(\d+)/, 1].to_i }
-    lazy_attr_reader(:member_name) {
-      at!('title').inner_text[%r!「#{Regexp.escape(title)}」/「(.+)」の(?:イラスト|漫画) \[pixiv\]!, 1]
-    }
-  end
-end
+require_relative 'pixiv_gem_monkey_patch'
 
 class ILovePixiv
   include Utils
