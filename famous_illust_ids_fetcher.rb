31656369ecda8250643a43369c8b7d3220b86b13
shimobayashi <akimasashimobayashi@gmail.com>
2016-04-30 11:11:02 +0900

    fix

--- famous_illust_ids_fetcher.rb
+++ famous_illust_ids_fetcher.rb
@@ -76,7 +76,7 @@ class FamousIllustIdsFetcher
         iter.next
       }
     }, proc{
-      illust_ids = count_by_illust_id.reject{|k, v| v < 5}.keys
+      illust_ids = count_by_illust_id.reject{|k, v| v < 2}.keys
       jobs = {}
       illust_ids.each {|illust_id|
         jobs[illust_id] = {name: :famous, score_threshold: 100}
